29fe591f3d701a42210b0053701c2b0341a5c47c
Eko Mirhard <eko.mirhard@gmail.com>
2021-10-13 12:42:53 +0700

    Added passes metadata

---
+++ collection/shop.js
@@ -0,0 +1,31 @@
+const passes = [{
+  id: 1,
+  discount: 0,
+  baseTokenURI: 'https://landdao.mypinata.cloud/ipfs/QmRuvtCferDdwh9cEZJjMVP8eZcK4B47rQqEhMMAhkiqxH',
+  price: '100e18',
+}, {
+  id: 2,
+  discount: 20,
+  baseTokenURI: 'https://landdao.mypinata.cloud/ipfs/QmPPYjS7CQT6pZZqrcWjmMLoLLcJYjeP2a7GNhBGdzoVmB',
+  price: '200e18',
+}, {
+  id: 3,
+  discount: 25,
+  baseTokenURI: 'https://landdao.mypinata.cloud/ipfs/QmeK9dcUfqnW7W8U4TFcYLXsVBKB9PUfN9HwyDCDr6gu2J',
+  price: '300e18',
+}, {
+  id: 4,
+  discount: 30,
+  baseTokenURI: 'https://landdao.mypinata.cloud/ipfs/QmWwFKczfD2obmbopUz4kbrdvRc9T4ojHjETa3DSFL8bJi',
+  price: '400e18',
+}, {
+  id: 5,
+  discount: 35,
+  baseTokenURI: 'https://landdao.mypinata.cloud/ipfs/QmTPAU7AntQrGUUAMGEdDMstzHnLaui2BEMwEE5Qc2CYpH',
+  price: '500e18',
+}, {
+  id: 6,
+  discount: 100,
+  baseTokenURI: 'https://landdao.mypinata.cloud/ipfs/QmPEahPbHuRMP6LWEz2GNbmDxiYaBqB14PuErSHFLitkty',
+  price: '1000e18',
+}];
